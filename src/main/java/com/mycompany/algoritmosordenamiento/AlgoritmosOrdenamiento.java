54fdbc7570c0a00a708db073f5a32bd647d1fc56
1062076461 <1062076461@PC22-SJ108.fup.edu>
2024-04-18 12:02:43 -0500

    algoritmoOrdenamiento

---
+++ src/main/java/com/mycompany/algoritmosordenamiento/AlgoritmosOrdenamiento.java
@@ -0,0 +1,84 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.algoritmosordenamiento;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author 1062076461
+ */
+public class AlgoritmosOrdenamiento {
+    
+    private static ArrayList <Atleta> atletas = new ArrayList();
+    static Scanner lec= new Scanner( System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Escribir el numero de tiempos a ingreesar ");
+        int input = lec.nextInt();
+        for(int i=0; i< input;i++){
+            System.out.println("Digite el tiempo del Atleta numero: "+i);
+            double tiempoAtleta=lec.nextDouble();
+            registrarTiempos(tiempoAtleta);
+            
+        }
+        System.out.println("Listado de los tiempos");
+        mostrarTiempos();
+        
+        int option=0;
+        do{
+            System.out.println("1. ordenar por seleccion");
+            System.out.println("2. ordenar por insercion");
+            System.out.println("3. ordenar Burbuja");
+            System.out.println("4. salir");
+            System.out.println("ingrese la opcion");
+            option=lec.nextInt();
+            switch(option){
+                case 1:
+                    break;
+                case 2:
+                    ordenarPorInsercion();
+                    mostrarTiempos();
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                default:
+                    System.out.println("opción no valida");
+                    break;
+                
+            }
+        }while (option!=4);
+    }
+    
+    public static void registrarTiempos(double tiempo){
+        Atleta nuevoTiempo = new Atleta(tiempo);
+        atletas.add(nuevoTiempo);
+    }
+    
+    public static void mostrarTiempos(){
+        for(Atleta a: atletas){
+            System.out.println(a.getTiempo()); 
+        }
+    }
+    public static void ordenarPorInsercion(){
+        for(int i=1; i<atletas.size();i++){
+            boolean agregado = false;
+            for(int j=i;j>0 && !agregado;j--){
+                Atleta uno = atletas.get(j);
+                Atleta dos = atletas.get(j-1);
+                if(uno.getTiempo()< dos.getTiempo()){
+                    atletas.set(j, dos);
+                    atletas.set(j-1, uno);
+                }else {
+                    agregado= true;
+                }
+            }
+        }
+    }
+    
+}
